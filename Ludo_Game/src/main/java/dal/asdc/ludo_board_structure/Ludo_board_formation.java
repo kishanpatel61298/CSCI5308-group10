4dd85216a9afae6b9fff691d8e3e28458d414792
Reshma Unnikrishnan <rs415638@dal.ca>
2021-03-06 15:30:58 +0530

    test cases

--- Ludo_Game/src/main/java/dal/asdc/ludo_board_structure/Ludo_board_formation.java
+++ Ludo_Game/src/main/java/dal/asdc/ludo_board_structure/Ludo_board_formation.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Ludo_board_formation {
     int [][] board = new int [15][15];
+    int board_value_count = 0;
     Map<String,Integer> movable_positions_map = new HashMap<String,Integer>();
     Map<String,Integer> safe_positions_map = new HashMap<String,Integer>();
     Map<String,Integer> winning_positions_map = new HashMap<String,Integer>();
@@ -15,14 +16,18 @@ public class Ludo_board_formation {
             for(int j=0;j<board[i].length;j++) {
                 board[i][j] = value;
                 value = value + 1;
+                board_value_count = board_value_count+1;
             }
         }
 
 //        for(int i=0;i<board.length;i++){
 //            for(int j=0;j<board[i].length;j++){
-//                System.out.println("board["+i+"]["+j+"]="+board[i][j]);
+//                //System.out.println("board["+i+"]["+j+"]="+board[i][j]);
+//                count = count+1;
 //            }
 //        }
+//        System.out.println("board count"+count);
+//        System.out.println("board count"+board.length);
     }
     public void set_movable_positions(){
         int count=0,count2= 0;
@@ -44,14 +49,15 @@ public class Ludo_board_formation {
                     count2 = count2 + 1;
                 }  }
         }
-        System.out.println(movable_positions_map.size());
+       // System.out.println(movable_positions_map.size());
 
     };
     public void set_safe_positions(){
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[i].length;j++) {
                 if(i==1&&j==8 || i==2&&j==6 || i==6&&j==1 || i==6&&j==12 || i==8&&j==2 || i==8&&j==13 || i==12&&j==8 || i==13&&j==6){
-                    System.out.println("board[" + i + "][" + j + "]=" + board[i][j]);
+                    //System.out.println("board[" + i + "][" + j + "]=" + board[i][j]);
+                	safe_positions_map.put("[" + i + "][" + j + "]",board[i][j]);
                 }
             }
         }
---
+++ Ludo_Game/src/test/java/dal/asdc/ludo_board_structure/LudoGameApplicationTest.java
@@ -0,0 +1,14 @@
+package dal.asdc.ludo_board_structure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LudoGameApplicationTest {
+
+//	@Test
+//	void testMain() {
+//		fail("Not yet implemented");
+//	}
+
+}
