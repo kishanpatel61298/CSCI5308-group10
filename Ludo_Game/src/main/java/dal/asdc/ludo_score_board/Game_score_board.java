d009757d5a003de9b1dd502a3fcf566aec2626f4
Reshma Unnikrishnan <rs415638@dal.ca>
2021-03-17 10:40:17 +0530

    score board package

---
+++ Ludo_Game/src/main/java/dal/asdc/ludo_score_board/Game_score_board.java
@@ -0,0 +1,17 @@
+package dal.asdc.ludo_score_board;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Game_score_board {
+		String db_driver = "com.mysql.jdbc.Driver";
+		String db_username="";
+	
+}
+
+
+
