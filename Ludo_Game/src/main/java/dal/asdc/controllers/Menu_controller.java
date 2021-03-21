1ca594a92311a9c6ab294118b2b71c89ce3a36ca
Reshma Unnikrishnan <rs415638@dal.ca>
2021-03-21 10:37:56 +0530

    Dashboard menu and game menu routing

--- Ludo_Game/src/main/java/dal/asdc/controllers/Menu_controller.java
+++ Ludo_Game/src/main/java/dal/asdc/controllers/Menu_controller.java
@@ -7,13 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import dal.asdc.ludo_menu.Dashboard_menu;
 import dal.asdc.model.Main_menu;
+import dal.asdc.model.Dash_menu;
+import dal.asdc.model.Game_menu;
 
 @Controller
 public class Menu_controller {
 	
+	Dashboard_menu dash_menu = new Dashboard_menu();
+	
 	@Autowired
 	Main_menu m_menu;
+	
+	@Autowired
+	Dash_menu ds_menu;
+	
+	@Autowired
+	Game_menu gm_menu;
+	
 	
 	@RequestMapping(value = "/")
 	public String menu(Model model) {
@@ -23,8 +35,24 @@ public class Menu_controller {
 
 	@RequestMapping(value="/d_menu", method=RequestMethod.POST)
 	public String dash_menu(@RequestParam("sel_menu") String smenu, Model model) {
+		model.addAttribute("Dash_menu", ds_menu);
 		model.addAttribute("smenu", smenu);
 		return "Dashboard_menu.jsp";
+	}
+	
+
+	@RequestMapping(value="/dash_menu", method=RequestMethod.POST)
+	public String decide_dash_menu(@RequestParam("dash_menu") String dmenu, Model model) {
+		model.addAttribute("dash_menu", dmenu);
+		String decide_dash = dash_menu.decide_dashboard_menu_page(Integer.parseInt(dmenu));
+		return decide_dash;
+	}
+	
+	@RequestMapping(value="/game_menu", method=RequestMethod.POST)
+	public String decide_game_menu(@RequestParam("game_menu") String gmenu, Model model) {
+		model.addAttribute("gamemenu", gmenu);
+		String decide_dash = dash_menu.decide_game_menu_page(Integer.parseInt(gmenu));
+		return decide_dash;
 	}
 	
 	
---
+++ Ludo_Game/src/main/java/dal/asdc/model/Game_menu.java
@@ -0,0 +1,16 @@
+package dal.asdc.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Game_menu {
+	public String game_menu;
+	 
+	public String getgame_menu() {
+		return game_menu;
+	}
+ 
+	public void setgame_menu(String game_menu) {
+		this.game_menu = game_menu;
+	}
+}
