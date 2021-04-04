30e31814669309a88fa8657b9700c9157bf0eda4
Prabhnoor Singh <pr870778@dal.ca>
2021-04-05 02:59:18 +0530

    added aggressive computer player class

---
+++ Ludo_Game/src/main/java/dal/asdc/game/Dice_user.java
@@ -0,0 +1,16 @@
+package dal.asdc.game;
+
+public abstract class Dice_user {
+    private static Dice_user unique_instace = null;
+
+    public static Dice_user instance(){
+        if( null == unique_instace){
+            unique_instace = new Dice();
+        }
+
+        return unique_instace;
+    }
+
+    public abstract int roll_dice();
+}
+
