1fc4097032d4d55aa1e5bca671b93d98aec53fc1
Soisde@123 <bhavsar>
2021-04-08 12:13:21 +0530

    Added password encryption

--- Ludo_Game/src/main/java/dal/asdc/login_register/Login.java
+++ Ludo_Game/src/main/java/dal/asdc/login_register/Login.java
@@ -1,5 +1,7 @@
 package dal.asdc.login_register;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import dal.asdc.dao.Player_dao;
 import dal.asdc.model.Player;
 
@@ -8,6 +10,18 @@ public class Login {
   public Player login(Player player) {
 	  Player_dao plyr_dao = new Player_dao();
 	  Player fatched_player = plyr_dao.filter_by_emailid(player.getPlayer_email());
-	  return fatched_player;
+	  if(fatched_player!=null &&  check_password(player.getPlayer_password(), fatched_player.getPlayer_password())) {
+		  return fatched_player;
+	  }
+	  else {
+		  return null;
+	  }
   }
+  
+  private boolean check_password(String plainPassword, String hashedPassword) {
+		if (BCrypt.checkpw(plainPassword, hashedPassword))
+			return true;			
+		else
+			return false;
+	}
 }
--- Ludo_Game/src/test/java/dal/asdc/login_register/Login_test.java
+++ Ludo_Game/src/test/java/dal/asdc/login_register/Login_test.java
@@ -3,6 +3,7 @@ package dal.asdc.login_register;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
 import dal.asdc.dao.Player_dao;
 import dal.asdc.model.Player;
@@ -12,10 +13,22 @@ public class Login_test {
 	@Test
 	 public void login_test() {
 		  Player player = new Player();
-		  player.setPlayer_email("test@gmail.com");
+		  boolean password_matched = false;
+		  player.setPlayer_email("admin@gmail.com");
 		  player.setPlayer_password("Admin@123");
 		  Player_dao plyr_dao = new Player_dao();
 		  Player fatched_player = plyr_dao.filter_by_emailid(player.getPlayer_email());
-	      assertEquals(fatched_player.getPlayer_email(), player.getPlayer_email()) ;		  
+	      assertEquals(fatched_player.getPlayer_email(), player.getPlayer_email()) ;
+	      password_matched = checkPass(player.getPlayer_password(), fatched_player.getPlayer_password());
+	      assertEquals(password_matched, true) ;
 	  }
+	
+	private boolean checkPass(String plainPassword, String hashedPassword) {
+		if (BCrypt.checkpw(plainPassword, hashedPassword))
+			return true;
+		else
+			return false;
+	}
+
 }
+
