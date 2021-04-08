1a99494e25a0af79de54f9789c823882c9dcac01
Prabhnoor Singh <pr870778@dal.ca>
2021-04-09 03:41:24 +0530

    updates to test

--- Ludo_Game/src/main/java/dal/asdc/player/IPlayer_intialiser.java
+++ Ludo_Game/src/main/java/dal/asdc/player/IPlayer_intialiser.java
@@ -6,4 +6,5 @@ public interface IPlayer_intialiser {
     void intialise(int number_of_players);
     Map<String, Player> getPlayer_list();
     void intialise_aggressive_computer_player();
+    void intialise_easy_computer_player();
 }
